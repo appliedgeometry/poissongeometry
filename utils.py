cb32315747ad1f731f99988267aff5136cdbc887
Miguel Evangelista <miguel.eva.alv@gmail.com>
2019-07-09 02:51:09 -0500

    add function validate_dimension

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from galgebra.ga import Ga
+from errors import DimensionError
 
 def dict_to_symbol_exp(dict, dimension, coordinates, dx=False):
     """This method converts a dictionary with symbolic values ​​in a symbolic expression
@@ -35,3 +38,12 @@ def variable_string(dimension, symbol):
             # return a type symbolic list
             return variable[0:len(variable) - 1]
 
+def validate_dimension(dimension):
+    """This method check if the dimension variable is valid for the this class"""
+    if not isinstance(dimension, int):
+        raise TypeError(F"Your variable is not a type int")
+
+    if dimension < 2:
+        raise DimensionError(F"Your dimension is not greater than two")
+    else:
+        return dimension
