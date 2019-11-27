6a37422fab13d7785a7c969a57b9f590fe754472
Miguel Evangelista <miguel.eva.alv@gmail.com>
2019-11-27 08:44:37 -0600

    update setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PoissonGeometry',
+    version='0.1',
+    scripts=['poi'] ,
+    author="Miguel Evangelista-Alvarado, Jose Ruíz, Pablo Suárez-Serrato",
+    author_email="miguel.eva.alv@gmail.com, jcpanta@im.unam.mx, pablo@im.unam.mx",
+    license="MIT License"
+    description="A Python class for (local) Poisson-Nijenhuis calculus on Poisson manifolds",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/javatechy/dokr",
+    packages=setuptools.find_packages(),
+    py_modules=['poisson'],
+    install_requires=['sympy', 'galgebra'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
