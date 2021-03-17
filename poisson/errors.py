096647b276eb9042b4da840c45281b53a9e6e3c1
Miguel Evangelista <miguel.eva.alv@gmail.com>
2021-03-17 15:58:36 -0600

    add the error file to create the custom errors to the  poissongeometry module

---
+++ poisson/errors.py
@@ -0,0 +1,46 @@
+"""
+    Copyright 2019 by P Suarez-Serrato, Jose Ruíz and M Evangelista-Alvarado.
+    Instituto de Matemáticas (UNAM-CU) México
+    This is free software; you can redistribute it and/or
+    modify it under the terms of the MIT License,
+    https://en.wikipedia.org/wiki/MIT_License.
+
+    This software has NO WARRANTY, not even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For more information about this project, you can see the paper
+    https://arxiv.org/abs/1912.01746
+
+    Thanks for citing our work if you use it.
+    @misc{evangelistaalvarado2019computational,
+        title={On Computational Poisson Geometry I: Symbolic Foundations},
+        author={M. A. Evangelista-Alvarado and J. C. Ruíz-Pantaleón and P. Suárez-Serrato},
+        year={2019},
+        eprint={1912.01746},
+        archivePrefix={arXiv},
+        primaryClass={math.DG}
+    }
+
+    This file contains the Errors Custom class to
+    use in the module Poisson geometry module
+"""
+
+
+class DimensionError(Exception):
+    pass
+
+
+class MultivectorError(Exception):
+    pass
+
+
+class FunctionError(Exception):
+    pass
+
+
+class DiferentialFormError(Exception):
+    pass
+
+
+class CasimirError(Exception):
+    pass
